8acdaeaf5761b1f8b2bcec848f7c31754beadf22
RudeTofu <68311570+3WokRide@users.noreply.github.com>
2025-10-14 13:19:10 +0800

    Initial commit

---
+++ src/main/java/com/seevrantillan/safespace/SafespaceApplication.java
@@ -0,0 +1,13 @@
+package com.seevrantillan.safespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seevrantillan/safespace/SafespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seevrantillan.safespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
