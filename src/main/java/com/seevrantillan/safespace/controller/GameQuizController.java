2b0705cd4bfab8b7ed62115b02173e8c56264e8a
RudeTofu <68311570+3WokRide@users.noreply.github.com>
2025-10-18 14:27:35 +0800

    added gameQuiz and progress Controllers

--- src/main/java/com/seevrantillan/safespace/controller/GameQuizController.java
+++ src/main/java/com/seevrantillan/safespace/controller/GameQuizController.java
@@ -1,5 +1,46 @@
 package com.seevrantillan.safespace.controller;
 
-public class GameQuizController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.seevrantillan.safespace.entity.GameQuizEntity;
+import com.seevrantillan.safespace.service.GameQuizService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/gamequizzes")
+public class GameQuizController {
+    private final GameQuizService service;
+
+    public GameQuizController(GameQuizService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/createGameQuiz")
+    public GameQuizEntity getMethodName(@RequestParam GameQuizEntity gameQuiz) {
+        return service.createGameQuiz(gameQuiz);
+    }
+
+    @GetMapping("/getAllGameQuizzes")
+    public List<GameQuizEntity> getAllGameQuizzes() {
+        return service.findAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public GameQuizEntity getGameQuizById(@PathVariable Long id) {
+        return service.findGameQuizById(id);
+    }
+
+    @PutMapping("/updateGameQuiz/{id}")
+    public GameQuizEntity updateGameQuiz(@PathVariable Long id, @RequestBody GameQuizEntity entity) {
+        return service.updateGameQuiz(id, entity);
+    }
 }
--- src/main/java/com/seevrantillan/safespace/controller/ProgressController.java
+++ src/main/java/com/seevrantillan/safespace/controller/ProgressController.java
@@ -1,5 +1,47 @@
 package com.seevrantillan.safespace.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.seevrantillan.safespace.entity.ProgressEntity;
+import com.seevrantillan.safespace.service.ProgressService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/progresses")
 public class ProgressController {
+    private final ProgressService service;
+
+    public ProgressController(ProgressService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/createProgress")
+    public ProgressEntity createProgress(@RequestBody ProgressEntity progress) {
+        return service.createProgress(progress);
+    }
+
+    @GetMapping("/getAllProgresses")
+    public List<ProgressEntity> getAllProgresses() {
+        return service.findAllProgress();
+    }
+
+    @GetMapping("/getProgress/{id}")
+    public ProgressEntity getProgressById(@PathVariable Long id) {
+        return service.findProgressById(id);
+    }
+
+    @PutMapping("/updateProgress/{id}")
+    public ProgressEntity updateProgress(@PathVariable Long id, @RequestBody ProgressEntity entity) {
+        return service.updateProgress(id, entity);
+    }
 
 }
